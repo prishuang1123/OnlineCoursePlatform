9cbbcf79207e57eeb99a8caeef82bcbcb068c441
pris <tintin851123@gmail.com>
2024-04-08 11:14:57 +0800

    "add order.cs"

---
+++ Project1/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; } // 訂單編號，主鍵
+
+        [ForeignKey("MemberID")]
+        public int MemberID { get; set; } // 會員編號，外鍵
+
+
+        [Required]
+        public DateTime OrderDate { get; set; } // 訂單日期
+        [Required]
+        public string OrderStatus { get; set; } // 訂單狀態
+        [Required]
+        public decimal TotalAmount { get; set; } // 訂單總額
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
