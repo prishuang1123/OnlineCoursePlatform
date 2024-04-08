0e7df60cba1afdbc2893cbf7050a1f3426784663
pris <tintin851123@gmail.com>
2024-04-08 12:05:05 +0800

    add discount,payment table, update order

---
+++ Project1/Models/Discount.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models
+{
+    public class Discount
+    {
+        [Key]
+        public int DiscountID { get; set; } // 折扣編號，主鍵
+        [Required]
+        public string DiscountName { get; set; } // 折扣名稱
+
+        public string? DiscountDESC { get; set; } // 折扣說明
+        [Required]
+        public double DiscountPercentage { get; set; } // 折扣百分比
+        [Required]
+        public DateTime StartDate { get; set; } // 折扣開始日
+        [Required]
+        public DateTime EndDate { get; set; } // 折扣結束日
+        [Required]
+        public bool Active { get; set; } // 是否活耀: 1/0
+        [Required]
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
---
+++ Project1/Models/Payment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project1.Models
+{
+    public class Payment
+    {
+        [Key]
+        public int PaymentID { get; set; } // 主鍵
+
+        
+        public int OrderID { get; set; } // 外鍵
+
+        [ForeignKey("OrderID")]
+        public Order Order { get; set; }
+
+        [Required]
+        public string PaymentMethod { get; set; } // 支付方式
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
