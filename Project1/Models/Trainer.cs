af0aa18e3d763f36dc4ec475d30ec1356a42d16a
Jun <yang12357958@gmail.com>
2024-05-08 14:38:02 +0800

    訓練師審核

--- Project1/Models/Trainer.cs
+++ Project1/Models/Trainer.cs
@@ -6,22 +6,29 @@ namespace Project1.Models
     public class Trainer
     {
 		[Key]
-		public int TrainerID { get; set; } // 訓練師編號，主鍵
+        [Display(Name = "訓練師ID")]
+        public int TrainerID { get; set; } // 訓練師編號，主鍵
 
-		public int MemberID { get; set; } // 會員ID，外鍵
+        [Display(Name = "會員ID")]
+        public int MemberID { get; set; } // 會員ID，外鍵
 
+        [Display(Name = "訓練師姓名")]
+        public string TrainerName { get; set; } // 名字
 
-		public string TrainerName { get; set; } // 名字
+        [Display(Name = "專長")]
+        public string Specialization { get; set; } // 專長
 
-		public string Specialization { get; set; } // 專長
+        [Display(Name = "經歷")]
+        public string Experience { get; set; } // 經歷
 
-		public string Experience { get; set; } // 經歷
+        [Display(Name = "資格")]
+        public string Qualifications { get; set; } // 資格
 
-		public string Qualifications { get; set; } // 資格
+        [Display(Name = "審核狀態")]
+        public string Status { get; set; } // 審核狀態，例如 "尚未審核"、"已審核"、"審核未通過"
 
-		public string Status { get; set; } // 審核狀態，例如 "尚未審核"、"已審核"、"審核未通過"
-
-		public string? Photo {  get; set; } //圖片
+        [Display(Name = "大頭貼")]
+        public string? Photo {  get; set; } //圖片
 
     }
 }
