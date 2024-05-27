0ecee620a9ffcad35f20a339bf0d125cacd8ee25
Shen Hua Tsai <a0975023117@gmail.com>
2024-05-27 15:57:39 +0800

    無法添加相通的課程與時段到購物車

--- Project1/Controllers/BrowseController.cs
+++ Project1/Controllers/BrowseController.cs
@@ -11,88 +11,88 @@ namespace Project1.Controllers
 {
     public class BrowseController : Controller
     {
-		private readonly ProjectDbContext _db;
+        private readonly ProjectDbContext _db;
         //internal DbSet<Trainer> trainerDbset;
         private int? memberId;
-		private IEnumerable<ShoppingCart> memberShoppingCart;
+        private IEnumerable<ShoppingCart> memberShoppingCart;
 
         public BrowseController(ProjectDbContext db)
-		{
-			_db = db;
-			//trainerDbset = _db.Set<Trainer>();
-		}
-		public async Task<IActionResult> Index()
         {
-			IEnumerable<Course>? courseObjList = await _db.Course.ToListAsync();
-			IEnumerable<CourseCategory>? categoryObjList = await _db.CourseCategory.ToListAsync();
+            _db = db;
+            //trainerDbset = _db.Set<Trainer>();
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Course>? courseObjList = await _db.Course.ToListAsync();
+            IEnumerable<CourseCategory>? categoryObjList = await _db.CourseCategory.ToListAsync();
 
-			//IQueryable<Trainer> queryTrainer = trainerDbset;
+            //IQueryable<Trainer> queryTrainer = trainerDbset;
 
-			if (courseObjList == null || categoryObjList == null)
-			{
-				return NotFound();
-			}
-			BrowseVM? browseVM = new BrowseVM()
-			{
-				courseList = courseObjList,
-				categoryList = categoryObjList,
-				
-			};
-			
-			return View(browseVM);
+            if (courseObjList == null || categoryObjList == null)
+            {
+                return NotFound();
+            }
+            BrowseVM? browseVM = new BrowseVM()
+            {
+                courseList = courseObjList,
+                categoryList = categoryObjList,
+
+            };
+
+            return View(browseVM);
         }
 
         // GET: Browse/Cart/5
         public async Task<IActionResult> ViewCart(int? id) //recieve memberID
-		{
-			//if (id==null || id == 0)
-			//{
-			//	return RedirectToAction ("Login", "Members");
-			//}
-			memberId = id;
-			//Course course= await _db.Course.Where(u=>u.CourseID==id).FirstOrDefaultAsync();
-			//select the member's cartItems to show all products added to the cart
-			
-			memberShoppingCart = await _db.Cart.Where(u => u.MemberID == 1).ToListAsync();
-			IEnumerable<int> courseIds= memberShoppingCart.Select(u => u.CourseID).ToList();
-			List<Course> courseObj=await _db.Course.Where(c=>courseIds.Contains(c.CourseID)).ToListAsync();
-			var courseObjList = memberShoppingCart
-				.GroupBy(c => c.CourseID)
-				.Select(cs => new
-				{
-					CourseID=cs.Key,
-					Quantity=cs.Sum(c => c.Quantity),
-                    CartIdToScheduleDate = cs.Select(c => new { CartID = c.CartID, ScheduleDate = _db.ClassSchedule.Where(s=>s.SchedulerID==c.SchedulerID).FirstOrDefault().Scheduler }).ToArray(),
+        {
+            //if (id==null || id == 0)
+            //{
+            //	return RedirectToAction ("Login", "Members");
+            //}
+            memberId = id;
+            //Course course= await _db.Course.Where(u=>u.CourseID==id).FirstOrDefaultAsync();
+            //select the member's cartItems to show all products added to the cart
+
+            memberShoppingCart = await _db.Cart.Where(u => u.MemberID == 1).ToListAsync();
+            IEnumerable<int> courseIds = memberShoppingCart.Select(u => u.CourseID).ToList();
+            List<Course> courseObj = await _db.Course.Where(c => courseIds.Contains(c.CourseID)).ToListAsync();
+            var courseObjList = memberShoppingCart
+                .GroupBy(c => c.CourseID)
+                .Select(cs => new
+                {
+                    CourseID = cs.Key,
+                    Quantity = cs.Sum(c => c.Quantity),
+                    CartIdToScheduleDate = cs.Select(c => new { CartID = c.CartID, ScheduleDate = _db.ClassSchedule.Where(s => s.SchedulerID == c.SchedulerID).FirstOrDefault().Scheduler }).ToArray(),
 
                 });
 
             decimal initSubtotal = 0;
             foreach (var courseId in courseIds)
-			{
-				initSubtotal += (courseObj.Where(c => c.CourseID == courseId).FirstOrDefault().Price) * (memberShoppingCart.Where(m => m.CourseID == courseId).FirstOrDefault().Quantity);
+            {
+                initSubtotal += (courseObj.Where(c => c.CourseID == courseId).FirstOrDefault().Price) * (memberShoppingCart.Where(m => m.CourseID == courseId).FirstOrDefault().Quantity);
             }
 
-			CartVM? cartVM = new CartVM()
-			{
-				memberId = (int)memberId,
+            CartVM? cartVM = new CartVM()
+            {
+                memberId = (int)memberId,
                 courseList = courseObj,
-				shoppingCartList= memberShoppingCart,
-                subtotal= initSubtotal,
+                shoppingCartList = memberShoppingCart,
+                subtotal = initSubtotal,
             };
-			
-			
 
-			if (cartVM != null)
-			{
-				
-				return View(cartVM);
-			}
-			else
-			{
-				return NotFound(); //login page?
-			}
 
-		}
+
+            if (cartVM != null)
+            {
+
+                return View(cartVM);
+            }
+            else
+            {
+                return NotFound(); //login page?
+            }
+
+        }
 
         public class AddToCartRequest
         {
@@ -117,9 +117,14 @@ namespace Project1.Controllers
 
             foreach (var scheduleID in request.SelectedSchedules)
             {
-                //var existingCartItem = await _db.Cart
-                //    .FirstOrDefaultAsync(c => c.CourseID == request.CourseID && c.SchedulerID == scheduleID && c.MemberID == 1); // 使用实际的 MemberID
-
+                var existingCartItem = await _db.Cart
+                    .FirstOrDefaultAsync(c => c.CourseID == request.CourseID && c.SchedulerID == scheduleID && c.MemberID == 1); // 使用实际的 MemberID
+                if (existingCartItem != null)
+                {
+                    return BadRequest("所選時段已在購物車中 不能重複添加相同的課程和時段");
+                }
+                else
+                {
                     var newCartItem = new ShoppingCart
                     {
                         CourseID = request.CourseID,
@@ -133,17 +138,7 @@ namespace Project1.Controllers
                         _db.Cart.Add(newCartItem);
                     }
 
-                //if (existingCartItem == null)
-                //{
-                //}
-                //else
-                //{
-                //    existingCartItem.Quantity += 1;
-                //    if (ModelState.IsValid)
-                //    {
-                //        _db.Cart.Update(existingCartItem);
-                //    }
-                //}
+                }
             }
 
             await _db.SaveChangesAsync();
@@ -154,70 +149,70 @@ namespace Project1.Controllers
 
 
         [HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> FollowItem(int? id) //courseID
-		{
-			//add the course into the logged in member's cart-> add a data into cart table
-			Course? courseObj = await _db.Course.Where(u => u.CourseID == id).FirstOrDefaultAsync();
-			if (courseObj != null)
-			{
-				//List<ShoppingCart> shoppingCartList = await _db.Cart.ToListAsync();
-				FollowItem followItem = await _db.FollowItem.Where(obj => obj.CourseID == id).FirstOrDefaultAsync();
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> FollowItem(int? id) //courseID
+        {
+            //add the course into the logged in member's cart-> add a data into cart table
+            Course? courseObj = await _db.Course.Where(u => u.CourseID == id).FirstOrDefaultAsync();
+            if (courseObj != null)
+            {
+                //List<ShoppingCart> shoppingCartList = await _db.Cart.ToListAsync();
+                FollowItem followItem = await _db.FollowItem.Where(obj => obj.CourseID == id).FirstOrDefaultAsync();
 
-				if (followItem == null)
-				{
-					FollowItem newFollowItem = new FollowItem();
-					newFollowItem.CourseID = courseObj.CourseID;
-					newFollowItem.MemberID = 1; //later with real memberID
+                if (followItem == null)
+                {
+                    FollowItem newFollowItem = new FollowItem();
+                    newFollowItem.CourseID = courseObj.CourseID;
+                    newFollowItem.MemberID = 1; //later with real memberID
 
-					if (ModelState.IsValid)
-					{
-						_db.FollowItem.Add(newFollowItem);
-						_db.SaveChanges();
-						TempData["success"] = "成功加入追蹤清單!!";
-						return RedirectToAction("Index", "Browse");
-					}
-					return RedirectToAction("Index", "Browse");
-				}
-				else
-				{					
-					TempData["success"] = "商品已在追蹤清單中!!";
-					return RedirectToAction("Index", "Browse");
-				}
-	
-			}
-			else
-			{
-				return NotFound();
-			}
+                    if (ModelState.IsValid)
+                    {
+                        _db.FollowItem.Add(newFollowItem);
+                        _db.SaveChanges();
+                        TempData["success"] = "成功加入追蹤清單!!";
+                        return RedirectToAction("Index", "Browse");
+                    }
+                    return RedirectToAction("Index", "Browse");
+                }
+                else
+                {
+                    TempData["success"] = "商品已在追蹤清單中!!";
+                    return RedirectToAction("Index", "Browse");
+                }
+
+            }
+            else
+            {
+                return NotFound();
+            }
 
 
-		}
+        }
 
-		[HttpGet]
-		public JsonResult CategoryNumber()
-		{
-			var categoryNum = _db.Course.GroupBy(c => c.CourseCategoryID)
-			.Select(cn => new
-			{
-				CourseCategoryID = cn.Key,
-				TotalNum = cn.Count(),
-				CategoryName = (_db.CourseCategory.FirstOrDefault(c => c.CourseCategoryID == cn.Key)).CourseCategoryName
-			});
+        [HttpGet]
+        public JsonResult CategoryNumber()
+        {
+            var categoryNum = _db.Course.GroupBy(c => c.CourseCategoryID)
+            .Select(cn => new
+            {
+                CourseCategoryID = cn.Key,
+                TotalNum = cn.Count(),
+                CategoryName = (_db.CourseCategory.FirstOrDefault(c => c.CourseCategoryID == cn.Key)).CourseCategoryName
+            });
 
-			//var categoryNumList = new List<object>();
-			//foreach (var categoryObj in categoryNum)
-			//{
-			//	categoryNumList.Add(categoryObj);
-			//}
-			return Json(categoryNum);
-		}
+            //var categoryNumList = new List<object>();
+            //foreach (var categoryObj in categoryNum)
+            //{
+            //	categoryNumList.Add(categoryObj);
+            //}
+            return Json(categoryNum);
+        }
         [HttpGet]
         public JsonResult GetClassSchedule()
         {
-			memberShoppingCart= _db.Cart.Where(u => u.MemberID == 1).ToList(); //member到時候再改
+            memberShoppingCart = _db.Cart.Where(u => u.MemberID == 1).ToList(); //member到時候再改
             var classSchedule = memberShoppingCart
-			.GroupBy(c => c.CourseID)
+            .GroupBy(c => c.CourseID)
             .Select(cs => new
             {
                 CourseID = cs.Key,
@@ -235,15 +230,15 @@ namespace Project1.Controllers
                                         on cart.SchedulerID equals schedule.SchedulerID
                                         select new { cart.SchedulerID, schedule.Scheduler })
                             .ToDictionary(x => x.SchedulerID, x => x.Scheduler),
-                
+
                 AllScheduleId = _db.ClassSchedule
-				.Where(obj=>obj.CourseID== cs.Key)
-				.Select(obj=>obj.SchedulerID)
-				.Distinct()
-				.ToList(),
+                .Where(obj => obj.CourseID == cs.Key)
+                .Select(obj => obj.SchedulerID)
+                .Distinct()
+                .ToList(),
                 AllScheduleDate = (from schedule in _db.ClassSchedule
-                                        where schedule.CourseID == cs.Key 
-                                        select new { schedule.SchedulerID, schedule.Scheduler })
+                                   where schedule.CourseID == cs.Key
+                                   select new { schedule.SchedulerID, schedule.Scheduler })
                             .ToDictionary(x => x.SchedulerID, x => x.Scheduler),
                 //AllScheduleDate = _db.ClassSchedule
                 //.Where(obj => obj.CourseID == cs.Key)
@@ -251,7 +246,7 @@ namespace Project1.Controllers
                 //.Distinct()
                 //.ToList(),
             });
-			
+
 
             return Json(classSchedule);
         }
@@ -259,15 +254,15 @@ namespace Project1.Controllers
         [HttpPost]
         public IActionResult UpdateQuantity(int quantity, int id)//quantity,cartId
         {
-			ShoppingCart cartItem = _db.Cart.Where(obj=>obj.CartID==id).FirstOrDefault();
-            cartItem.Quantity= quantity;
-			_db.SaveChanges();
+            ShoppingCart cartItem = _db.Cart.Where(obj => obj.CartID == id).FirstOrDefault();
+            cartItem.Quantity = quantity;
+            _db.SaveChanges();
             //TempData["success"] = "成功更新商品數量!!";
-			Course course = _db.Course.Where(c=>c.CourseID==cartItem.CourseID).FirstOrDefault();
-			
+            Course course = _db.Course.Where(c => c.CourseID == cartItem.CourseID).FirstOrDefault();
+
             decimal totalPrice = (course.Price) * quantity;
-            
-            
+
+
             return Json(new { TotalPrice = totalPrice.ToString("c") });
         }
 
@@ -278,16 +273,16 @@ namespace Project1.Controllers
             ShoppingCart cartItem = _db.Cart.Where(obj => obj.CartID == id).FirstOrDefault();
             _db.Cart.Remove(cartItem);
             _db.SaveChanges();
-			int memberId = cartItem.MemberID;
+            int memberId = cartItem.MemberID;
             TempData["success"] = "商品刪除成功!!";
-			//return RedirectToAction("ViewCart", "Browse", new { id = 1 });
-			return Json(new { success = true, memberId = memberId });
-		}
+            //return RedirectToAction("ViewCart", "Browse", new { id = 1 });
+            return Json(new { success = true, memberId = memberId });
+        }
 
         [HttpGet]
         public async Task<IActionResult> UpdateSubtotal()
         {
-			decimal subtotal = 0;
+            decimal subtotal = 0;
             memberShoppingCart = await _db.Cart.Where(u => u.MemberID == 1).ToListAsync();
             IEnumerable<int> courseIds = memberShoppingCart.Select(u => u.CourseID).ToList();
             List<Course> courseObj = await _db.Course.Where(c => courseIds.Contains(c.CourseID)).ToListAsync();
@@ -300,60 +295,60 @@ namespace Project1.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> ValidateDiscountCode(string? discountCode) 
-		{
-			string validationResult = "";
-			double discountPercentage = 0;
+        public async Task<IActionResult> ValidateDiscountCode(string? discountCode)
+        {
+            string validationResult = "";
+            double discountPercentage = 0;
 
-            Discount discountObj=await _db.Discount.Where(d=>d.DiscountName == discountCode).FirstOrDefaultAsync();
-			if (discountObj != null)
-			{
+            Discount discountObj = await _db.Discount.Where(d => d.DiscountName == discountCode).FirstOrDefaultAsync();
+            if (discountObj != null)
+            {
                 DateTime now = DateTime.Now;
-                if (discountObj.StartDate < now && now < discountObj.EndDate && discountObj.Active==true)
-				{
+                if (discountObj.StartDate < now && now < discountObj.EndDate && discountObj.Active == true)
+                {
                     discountPercentage = discountObj.DiscountPercentage;
                     validationResult = "success";
                 }
-				else
-				{
-					discountPercentage = 1;
+                else
+                {
+                    discountPercentage = 1;
                     validationResult = "not applicable";
-                } 
+                }
 
             }
-			else
-			{
+            else
+            {
                 discountPercentage = 1;
                 validationResult = "failed";
             }
 
-			return Json(new { validationResult = validationResult, discountPercentage= discountPercentage });
-		}
+            return Json(new { validationResult = validationResult, discountPercentage = discountPercentage });
+        }
 
-   //     [HttpPost]
-   //     public IActionResult UpdateCartItem(int courseId, int scheduleId, bool isSelected)//update cartItem
-   //     {
-   //         int memberId = _db.Cart.Where(obj => obj.CourseID == courseId).FirstOrDefault().MemberID;
-   //         if (isSelected)
-			//{
-   //             ShoppingCart cartItem = _db.Cart.Where(obj => obj.SchedulerID == scheduleId).FirstOrDefault();
-   //             _db.Remove(cartItem);
-   //             _db.SaveChanges();
-   //         }
-			//else
-			//{
-			//	ShoppingCart newCartItem = new ShoppingCart();
+        //     [HttpPost]
+        //     public IActionResult UpdateCartItem(int courseId, int scheduleId, bool isSelected)//update cartItem
+        //     {
+        //         int memberId = _db.Cart.Where(obj => obj.CourseID == courseId).FirstOrDefault().MemberID;
+        //         if (isSelected)
+        //{
+        //             ShoppingCart cartItem = _db.Cart.Where(obj => obj.SchedulerID == scheduleId).FirstOrDefault();
+        //             _db.Remove(cartItem);
+        //             _db.SaveChanges();
+        //         }
+        //else
+        //{
+        //	ShoppingCart newCartItem = new ShoppingCart();
 
-			//	newCartItem.CourseID = courseId;
-			//	newCartItem.SchedulerID = scheduleId;
-   //             newCartItem.MemberID=memberId;
-			//	newCartItem.Quantity=1;
-   //             _db.Add(newCartItem);
-   //             _db.SaveChanges();
-   //         }
-            
-   //         return Json(new { success=true });
-   //     }
+        //	newCartItem.CourseID = courseId;
+        //	newCartItem.SchedulerID = scheduleId;
+        //             newCartItem.MemberID=memberId;
+        //	newCartItem.Quantity=1;
+        //             _db.Add(newCartItem);
+        //             _db.SaveChanges();
+        //         }
+
+        //         return Json(new { success=true });
+        //     }
 
         [HttpPost]
         public IActionResult UpdateCartItem(int courseId, int scheduleId, bool isSelected)
@@ -383,7 +378,7 @@ namespace Project1.Controllers
                             _db.Remove(cartItem);
                             _db.SaveChanges();
                         }
-                        
+
                     }
 
                     return Json(new { success = true });
