ec9d4d3185085c0ddd1c8dd4c9cf7fb948f5e592
Simon <a0975023117@gmail.com>
2024-05-28 11:33:52 +0800

    從Member資料表移除帳號密碼欄位與是否成為管理員欄位

--- Project1/Models/Member.cs
+++ Project1/Models/Member.cs
@@ -27,14 +27,8 @@ namespace Project1.Models
 		[Required]
 		public bool? IsTrainer { get; set; } // 是否為訓練師，1/0
 
-		
-		public bool? IsAdministrator { get; set; } // 是否為系統管理員，1/0
 
 		public string? Photo { get; set; }
-
-		public string? Account { get; set; }
-
-		public string? Password { get; set; }
 
 		[Required]
 		public string Address { get; set; }
